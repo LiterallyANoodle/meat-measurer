530831589d0110d6a70279fc373c9ffdd1cb4b7e
Matthew Mahan <themaskedkid321@gmail.com>
2023-09-26 17:01:31 -0500

    experiments with getters and setters

--- meat-measurer/src/main.rs
+++ meat-measurer/src/main.rs
@@ -42,15 +42,60 @@ mod menu {
 mod recipe {
 
     #[derive(Debug)]
+    pub struct Recipe {
+        bulk_components: Vec<Ingredient>,
+        micro_components: Option<Vec<Ingredient>>
+    }
+
+    impl Recipe {
+        
+        pub fn new(bulk_components: Vec<Ingredient>, micro_components: Option<Vec<Ingredient>>) -> Recipe {
+            return Recipe { bulk_components: bulk_components, micro_components: micro_components };
+        }
+
+        pub fn get_bulk_components(&self) -> &Vec<Ingredient> {
+            return &self.bulk_components;
+        }
+
+        pub fn get_micro_components(&self) -> &Option<Vec<Ingredient>> {
+            return &self.micro_components;
+        }
+
+        // pub fn set_bulk_components(&self, bulk_components: Vec<Ingredient>) -> Self {
+        //     let micro_components = self.get_micro_components();
+        //     return Recipe { bulk_components: bulk_components, micro_components: micro_components }
+        // }
+
+    }
+
+    #[derive(Debug)]
     pub struct Ingredient {
-        pub name: Box<String>,
-        pub percent: Box<f32>
+        name: String,
+        percent: f32
     }
 
     impl Ingredient {
+
         pub fn new(name: String, percent: f32) -> Ingredient {
-            Ingredient { name: Box::new(name), percent: Box::new(percent) }
+            return Ingredient { name: name, percent: percent }
         }
+
+        pub fn get_name(&self) -> &str {
+            return &self.name;
+        }
+
+        pub fn get_percent(&self) -> &f32 {
+            return &self.percent;
+        }
+
+        pub fn set_name(&self, name: String) -> Self {
+            return Self::new(name, *self.get_percent());
+        }
+
+        pub fn set_percent(&self, percent: f32) -> Self {
+            return Self::new(self.get_name().to_string(), percent);
+        }
+
     }
 
 }
@@ -59,9 +104,12 @@ fn main() -> io::Result<()> {
 
     let question = "What is your ingredient name?";
     let input = menu::ask_question(&question).unwrap();
-    let my_ingredient = recipe::Ingredient::new(input.trim().to_string(), 50f32);
-    println!("You typed {}", my_ingredient.name);
-    println!("Your ingredient is {} with {}%", my_ingredient.name, my_ingredient.percent);
+    let mut my_ingredient = recipe::Ingredient::new(input.trim().to_string(), 50f32);
+    println!("You typed {}", my_ingredient.get_name());
+    println!("Your ingredient is {} with {}%", my_ingredient.get_name(), my_ingredient.get_percent());
+
+    my_ingredient = my_ingredient.set_percent(30f32);
+    println!("But I think I'll have {} with {}% instead!", my_ingredient.get_name(), my_ingredient.get_percent());
 
     let test_list = vec!["crazy!", "i stay noided", "what the dog doin?"];
     let list_response = menu::ask_list(test_list, "Are you listening to fucking hyperpop you fucking furry? (type a number)");
